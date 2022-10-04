aa561f7e73325f9f9dae5c0e88692857ae851e8e
RichardVargasPilarte <ricardo.pilarte94@gmail.com>
2022-10-04 10:27:10 -0600

    import routes index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
-import router from './routes'
+import router from './routes/index'
 
 require('dotenv').config();
 
